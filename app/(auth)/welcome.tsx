c5af0200d03682dd113dd5046ed81f2f57067951
hrishikasamani <samani.hr@northeastern.edu>
2024-12-30 15:20:56 -0500

    onboarding navigation stack complete

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
@@ -8,6 +9,8 @@ import Swiper from "react-native-swiper";
 const Onboarding = () => {
     const swiperRef = useRef<Swiper>(null);
     const [activeIndex, setActiveIndex] = useState(0);
+    const isLastSlide = activeIndex === onboarding.length-1;
+
     return (
         <SafeAreaView className="flex h-full items-center justify-between bg-white">
             <TouchableOpacity onPress={() => {
@@ -25,14 +28,14 @@ const Onboarding = () => {
                 onIndexChanged={(index) => setActiveIndex(index)}
                 >
                     {onboarding.map((item) => (
-                        <View key={item.id} className="flex item-center justify-center p-5">
+                        <View key={item.id} className="flex items-center justify-center p-5">
                             <Image
                                 source={item.image}
                                 className="w-full h-[300px]"
                                 resizeMode="contain"
                             />
                             <View className="flex flex-row items-center justify-center w-full mt-10">
-                                <Text className="text-black text-3xl font-bold max-10">
+                                <Text className="text-black text-3xl font-bold max-10 text-center">
                                     {item.title}
                                 </Text>
                             </View>
@@ -42,6 +45,14 @@ const Onboarding = () => {
                         </View>
                     ))}
             </Swiper>
+            <CustomButton 
+                title = {isLastSlide ? "Get Started" : "Next"}
+                onPress={() =>
+                    isLastSlide
+                    ? router.replace("/(auth)/sign-up")
+                    : swiperRef.current?.scrollBy(1)
+                }
+                className="w-11/12 mb-10" />
         </SafeAreaView>
     )
 };
